fbe6434aee406c72447c55c9cf338339b54351d0
rensarmiento <54691646+rensarmiento@users.noreply.github.com>
2021-11-12 21:22:04 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import './stylesheet/app.scss'
-import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube, FaPhoneAlt, FaEnvelope, FaMapMarker } from "react-icons/fa";
-
+import "./stylesheet/app.scss";
+//import firebaseDB from "./firebase";
+import React, { useState } from "react";
+import firebaseDB from "./firebase";
 function App() {
+  const [images, setImages] = useState([]);
+
+  const handleChange = (e) => {
+    for (let i = 0; i < e.target.files.length; i++) {
+      const newImage = e.target.files[i];
+      newImage["id"] = Math.random();
+      setImages((prevState) => [...prevState, newImage]);
+    }
+  };
+
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const { firstName, lastName, email, phone, message } = state;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!firstName || !lastName || !email || !phone || !message) {
+      console.log("Please provide value in each input field");
+    } else {
+      firebaseDB.child("contacts").push(state);
+      alert("file upload success");
+      setState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        message: "",
+      });
+      document.getElementById("image").value = "";
+    }
+  };
+
+  const handleInputChange = (e) => {
+    let { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
   return (
     <div className="App">
       <header>
@@ -9,74 +52,77 @@ function App() {
         <link rel="icon" href="/favicon.ico" />
       </header>
 
-      <h1>Contact Us</h1>
-      <p className="sub-title">lorem ipsum</p>
+      <h1>Embarrassing Image Uploader</h1>
+      <p className="sub-title">
+        Send embarrassing photos to your loved one's significant other.
+      </p>
 
       <div id="contact-container">
-        <div className="contact-info">
-          <h4>
-            Contact Information
-          </h4>
-          <p>Fill up the form and then click send</p>
-          <div className="icon-text">
-            <i><FaPhoneAlt className="fa"></FaPhoneAlt></i>
-            <span>123-456-7890</span>
-          </div>
-          <div className="icon-text">
-            <i><FaEnvelope className="fa"></FaEnvelope></i>
-            <span>email@email.com</span>
-          </div>
-          <div className="icon-text">
-          <i><FaMapMarker className="fa"></FaMapMarker></i>
-            <span>123 Street Ave</span>
-          </div>
-          <div className="social-media">
-            <a href="#" className="icon-circle">
-              <i><FaFacebookF className="fa"></FaFacebookF></i>
-            </a>
-            <a href="#" className="icon-circle">
-              <i><FaTwitter className="fa"></FaTwitter></i>
-            </a>
-            <a href="#" className="icon-circle">
-              <i><FaInstagram className="fa"></FaInstagram></i>
-            </a>
-            <a href="#" className="icon-circle">
-              <i><FaYoutube className="fa"></FaYoutube></i>
-            </a>  
-            </div>
-        </div>
-        <form>
+        <form className="form" onSubmit={handleSubmit}>
           <div className="col">
             <div className="form-group">
               <label>First Name</label>
-              <input type="text"></input>
+              <input
+                type="text"
+                className="form-control"
+                name="firstName"
+                onChange={handleInputChange}
+                value={firstName}
+              ></input>
             </div>
             <div className="form-group">
               <label>Last Name</label>
-              <input type="text"></input>
+              <input
+                type="text"
+                className="form-control"
+                name="lastName"
+                onChange={handleInputChange}
+                value={lastName}
+              ></input>
             </div>
           </div>
           <div className="col">
             <div className="form-group">
               <label>Email</label>
-              <input type="email"></input>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                onChange={handleInputChange}
+                value={email}
+              ></input>
             </div>
             <div className="form-group">
               <label>Phone Number</label>
-              <input type="tel"></input>
+              <input
+                type="tel"
+                className="form-control"
+                name="phone"
+                onChange={handleInputChange}
+                value={phone}
+              ></input>
             </div>
           </div>
-          
+
           <div className="col">
             <div className="form-group solo">
               <label>Message</label>
-              <textarea></textarea>
+              <textarea
+                type="text"
+                className="form-control"
+                name="message"
+                onChange={handleInputChange}
+                value={message}
+              ></textarea>
             </div>
           </div>
-          {/* <FileUploadComponent /> */}
+          {/*<FileUploadComponent />*/}
+          <input type="file" id="image" onChange={handleChange} />
           <div className="col">
             <div className="form-group solo right">
-              <button className="primary">Send Message</button>
+              <button className="primary" type="submit">
+                Send Message
+              </button>
             </div>
           </div>
         </form>
